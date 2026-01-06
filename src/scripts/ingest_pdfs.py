155c775cc1cd32a2a96a655654a96ecee7473c72
dfarhangi <dan.farhangi@gmail.com>
2026-01-06 11:19:55 +0330

    edited ingest_pdfs script

--- src/scripts/ingest_pdfs.py
+++ src/scripts/ingest_pdfs.py
@@ -2,10 +2,9 @@ import os
 from typing import List
 
 from dotenv import load_dotenv
-
 from langchain_community.document_loaders import PyPDFLoader
-from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 try:
     from langchain_chroma import Chroma
